bf8138edf6e181178c4d43eaf7a3012a2a86c205
xzient <31088051+xzient@users.noreply.github.com>
2018-10-13 15:32:28 -0400

    Add Firebase Authentication to SignUp

--- app/src/main/java/cmsc424/cmsc424_pg11/SignUp.java
+++ app/src/main/java/cmsc424/cmsc424_pg11/SignUp.java
@@ -1,5 +1,6 @@
 package cmsc424.cmsc424_pg11;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -18,6 +19,11 @@ import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.safetynet.SafetyNet;
 import com.google.android.gms.safetynet.SafetyNetApi;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import org.w3c.dom.Text;
 
@@ -31,36 +37,49 @@ public class SignUp extends AppCompatActivity
     final String SecretKey  = "6LfFrXQUAAAAAIzzjOSE4posQ6AeZp7kk7dLiyA7";
     private boolean passCaptcha = false;
 
+    //Authentication
+    private FirebaseAuth mAuth;
+
+
+    //Accept Terms and Conditions
+    CheckBox acceptTerms;
+
+    //General Sign Up
     Button btnRequest;
     TextView tvResult;
-    CheckBox acceptTerms;
     Button signUpButton;
     EditText name;
     EditText email;
     EditText password1;
-    EditText passwword2;
+    EditText password2;
+
+    //TAG
+    final String TAG = "SignUp Activity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        //Authentication
+        mAuth = FirebaseAuth.getInstance();
+
         //Buttons and EditTexts
         signUpButton = findViewById(R.id.signUpButton);
-        name = findViewById(R.id.editTextEmail);
-        email = findViewById(R.id.editTextName);
+        name = findViewById(R.id.editTextName);
+        email = findViewById(R.id.editTextEmail);
         password1 = findViewById(R.id.editTextPassword1);
-        passwword2 = findViewById(R.id.editTextPassword2);
+        password2 = findViewById(R.id.editTextPassword2);
+
+            //actions
+        signUpButton.setOnClickListener(RegisterOnClickListener);
 
 
         //CAPTCHA init
         tvResult = findViewById(R.id.result);
         btnRequest = findViewById(R.id.request);
         btnRequest.setOnClickListener(RqsOnClickListener);
-
-        //Terms and Services
-        acceptTerms = findViewById(R.id.acceptTerms);
-        acceptTerms.setChecked(false);
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(SafetyNet.API)
@@ -70,11 +89,68 @@ public class SignUp extends AppCompatActivity
 
         mGoogleApiClient.connect();
 
+        //Terms and Services
+        acceptTerms = findViewById(R.id.acceptTerms);
+        acceptTerms.setChecked(false);
+
+
+
 
 
     }//End onCreate
 
 
+    /**
+     * Click Listener for Sign Up / Register
+     */
+    View.OnClickListener RegisterOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            //Get values from TextEdits
+            String newEmail = email.getText().toString();
+            String newPassword = password1.getText().toString();
+
+
+            mAuth.createUserWithEmailAndPassword(newEmail, newPassword)
+                    .addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(SignUp.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                updateUI(null);
+                            }
+
+                            // ...
+                        }
+                    });
+
+
+        }
+    };
+
+    public void updateUI(FirebaseUser user) {
+        if(user != null) {
+            startActivity(new Intent(SignUp.this, SignIn.class));
+            //Eventually this should take you to email verification
+            //TO DO
+        }
+        else {
+            Toast.makeText(this, "Not Signed User", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Click Listener for CAPTCHA
+     */
     View.OnClickListener RqsOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
@@ -123,6 +199,18 @@ public class SignUp extends AppCompatActivity
 
         }
     };
+
+
+    //onStart may not be needed for Sign Up
+    /*
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser); //Create function to update
+    }
+    */
 
 
 
