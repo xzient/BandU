496ce3e247498889cdbc6a161ba870d1bd10a24a
xzient <31088051+xzient@users.noreply.github.com>
2018-12-05 15:09:53 -0500

    Add subscription fully implemented

--- app/src/main/java/cmsc424/cmsc424_pg11/RecyclerViewAdapterSubscriptions.java
+++ app/src/main/java/cmsc424/cmsc424_pg11/RecyclerViewAdapterSubscriptions.java
@@ -12,22 +12,45 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.daimajia.swipe.SwipeLayout;
+import com.daimajia.swipe.adapters.RecyclerSwipeAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class RecyclerViewAdapterSubscriptions extends RecyclerView.Adapter<RecyclerViewAdapterSubscriptions.ViewHolder>{
+
+public class RecyclerViewAdapterSubscriptions extends RecyclerSwipeAdapter<RecyclerViewAdapterSubscriptions.ViewHolder>{
 
     private static final String TAG = "RecyclerViewAdapterSubs";
 
-    private ArrayList<String> mGenres = new ArrayList<>();
+    private ArrayList<String> mValues = new ArrayList<>();
     private Context mContext;
+    private ArrayList<String> mValueIdList;
+    private boolean mIsLocation;
+    FirebaseUser mCurrentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+    DatabaseReference mDataBase;
 
-    public RecyclerViewAdapterSubscriptions(Context mContext, ArrayList<String> mGenres) {
+    public RecyclerViewAdapterSubscriptions(Context mContext, ArrayList<String> mmValues, ArrayList<String> mValueIdList, boolean mIsLocation) {
 
 
-        this.mGenres = mGenres;
+        this.mValues = mmValues;
         this.mContext = mContext;
+        this.mValueIdList = mValueIdList;
+        this.mIsLocation = mIsLocation;
+        if(mIsLocation) {
+            this.mDataBase = FirebaseDatabase.getInstance().getReference("server").child("cityuser");
+        }
+        else {
+            this.mDataBase = FirebaseDatabase.getInstance().getReference("server").child("genreuser");
+        }
     }
 
     @NonNull
@@ -39,46 +62,145 @@ public class RecyclerViewAdapterSubscriptions extends RecyclerView.Adapter<Recyc
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+    public void onBindViewHolder(@NonNull final ViewHolder holder, final int position) {
         Log.d(TAG, "onBindViewHolder: called");
 
-        holder.genreAdapter.setText(mGenres.get(position));
+        holder.genreAdapter.setText(mValues.get(position));
+
+        holder.parentCardView.setShowMode(SwipeLayout.ShowMode.PullOut);
+
+        //Right swipe
+        holder.parentCardView.addDrag(SwipeLayout.DragEdge.Right, holder.parentCardView.findViewById(R.id.right_swipe));
 
 
-
-
-
-        //Listener
-
-        /*
         holder.parentCardView.setOnClickListener(new View.OnClickListener() {
+
+            boolean wasClosed = true;
             @Override
             public void onClick(View view) {
-                Log.d(TAG, "onClick: clicked on:"+ mGenres.get(position));
+                Log.d(TAG, "onClick: clicked on:"+ mValues.get(position));
+
+                if (SwipeLayout.Status.Close == holder.parentCardView.getOpenStatus())
+                {
+                    if (wasClosed)
+                    {
+
+                        holder.parentCardView.open();
+                    }
+                    else
+                    {
+                        wasClosed = true;
+                        //holder.parentCardView.close();
+                    }
+                }
+                else
+                {
+                    wasClosed = false;
+                    //holder.parentCardView.close();
+                }
             }
         });
-        */
+
+
+
+        holder.parentCardView.addSwipeListener(new SwipeLayout.SwipeListener() {
+            @Override
+            public void onStartOpen(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onOpen(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onStartClose(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onClose(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onUpdate(SwipeLayout layout, int leftOffset, int topOffset) {
+
+            }
+
+            @Override
+            public void onHandRelease(SwipeLayout layout, float xvel, float yvel) {
+
+            }
+        });
+
+        holder.delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                mDataBase.child(mValueIdList.get(position)).child(mCurrentFirebaseUser.getUid()).removeValue();
+
+
+
+
+                //Remove from display
+                mItemManger.removeShownLayouts(holder.parentCardView);
+                mValues.remove(position);
+                mValueIdList.remove(position);
+                notifyItemRemoved(position);
+                notifyItemRangeChanged(position, mValues.size());
+                mItemManger.closeAllItems();
+
+                //Toast.makeText(view.getContext(), "Unsubscribed to genre!", Toast.LENGTH_SHORT).show();
+
+                if (mIsLocation) {
+                    Toast.makeText(view.getContext(), "Unsubscribed to location!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(view.getContext(), "Unsubscribed to genre!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
 
     }
 
     @Override
     public int getItemCount() {
-        return mGenres.size();
+        return mValues.size();
+    }
+
+    @Override
+    public int getSwipeLayoutResourceId(int position) {
+        return 0;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
 
         TextView genreAdapter;
-        CardView parentCardView;
+        ImageView delete;
+        SwipeLayout parentCardView;
 
         public ViewHolder(View itemView) {
             super(itemView);
 
 
+            delete = itemView.findViewById(R.id.delete_swipe);
             genreAdapter = itemView.findViewById(R.id.recycler_view_item_subscription_genre);
             parentCardView = itemView.findViewById(R.id.parent_card_view_subscription);
 
         }
+    }
+
+    public void updateList(List<String> newListGenres, List<String> newListIDs) {
+        mValueIdList = new ArrayList<>();
+        mValues = new ArrayList<>();
+
+        mValues.addAll(newListGenres);
+        mValueIdList.addAll(newListIDs);
+        notifyDataSetChanged();
     }
 }
